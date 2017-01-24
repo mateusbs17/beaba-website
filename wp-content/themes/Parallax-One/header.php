12ea3a808121326044c3a0d25b67c78517a79676
Mateus Batista <mateus@institutosoma.org.br>
2017-01-24 12:06:57 -0200

    Adicionando favicon

--- wp-content/themes/Parallax-One/header.php
+++ wp-content/themes/Parallax-One/header.php
@@ -14,6 +14,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta property="og:image" content="http://www.danielbatista.com.br/wp-content/uploads/2017/01/siteai-21.png" />
+<link rel="shortcut icon" type="image/png" href="http://www.danielbatista.com.br/wp-content/uploads/2017/01/favicon.ico"/>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php parallax_hook_head_bottom(); ?>
